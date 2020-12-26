ac92e2eff95b331754862c4a7b35cd2964141902
LHuartNisgroup <l.huart@nis-group.fr>
2020-12-26 15:08:04 +0100

    instalation de dotenv qui permet d'utilisé les variable de .env et création d'une structure MVC

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const path = require("path");
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config()
 
 const initRoute = require("./src/route.js");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.APP_PORT || 3000;
 
 let publicDisPath = path.join(__dirname, "./public");
 app.use(express.static(publicDisPath));
 
+app.set("views", path.join(__dirname, "./src/views"));
 app.set("view engine", "ejs");
 
-mongoose.connect('mongodb://root:Endemik.2020@endemikmongo.xn--pange-esa.site/admin', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://"+process.env.DB_USERNAME+":"+process.env.DB_PASSWORD+"@"+process.env.DB_HOST+"/admin", {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect('mongodb://root:Endemik.2020@endemikmongo.xn--pange-esa.site/admin', {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
