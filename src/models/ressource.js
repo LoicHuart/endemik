78456c9b08759f0fff1bb88ca15a3e5c5b2b4096
LHuartNisgroup <l.huart@nis-group.fr>
2021-02-09 17:08:00 +0100

    modif model cotegorie_ressource, conge, ressource, service

--- src/models/ressource.js
+++ src/models/ressource.js
@@ -3,13 +3,13 @@ const { Schema } = mongoose;
 
 const ressourceSchema = new mongoose.Schema({
     _id: Schema.Types.ObjectId,
-    title:  String,
-    date: Date,
-    url_doc: String,
-    active: Boolean,
-    id_categorie: Schema.Types.ObjectId,
-    id_salarie_createur: Schema.Types.ObjectId,
-    id_service: { type: Schema.Types.ObjectId, default: null }
+    title: { type: String, required: true },
+    date: { type: Date, required: true },
+    url_doc: { type: String, required: true },
+    active: { type: Boolean, required: true },
+    id_categorie: { type: Schema.Types.ObjectId, required: true },
+    id_salarie_createur: { type: Schema.Types.ObjectId, required: true },
+    id_service: { type: Schema.Types.ObjectId, default: null, required: true }
 });
 
 module.exports = mongoose.model('ressource', ressourceSchema, 'ressource');  
