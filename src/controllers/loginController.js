aa9f8052e653797256c06cd9fceb3b26ec46402c
LHuartNisgroup <l.huart@nis-group.fr>
2021-02-10 10:22:33 +0100

    fix bug login

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -2,14 +2,14 @@ var SalarieSchema = require("../models/salarie");
 
 var LoginController = {
   async index(req, res) {
-    res.render("pages/login");
+    res.render("pages/login/login");
   },
 
   async auth(req, res) {
     var email = req.body.email;
     var password = req.body.password;
     if (email && password) {
-      salarieSchema.find(
+      SalarieSchema.find(
         { mail: email, mdp: password },
         function (error, results, fields) {
           if (results.length > 0) {
@@ -17,7 +17,7 @@ var LoginController = {
             req.session.user = results[0];
             res.redirect("/tableauDeBord");
           } else {
-            res.render("pages/login", {
+            res.render("pages/login/login", {
               error: "Email ou mot de passe incorrect",
             });
           }
@@ -25,7 +25,7 @@ var LoginController = {
         }
       );
     } else {
-      res.render("pages/login", {
+      res.render("pages/login/login", {
         error: "Entrer une adresse Email et un mot de passe s'il vous plaît",
       });
       res.end();
