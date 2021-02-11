74d1b9a3c59acbf5daf362be41e241ce4f385db8
LHuartNisgroup <l.huart@nis-group.fr>
2021-02-11 09:08:43 +0100

    add service

---
+++ src/models/Service.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+//majuscules
+const ServiceSchema = new mongoose.Schema({
+  _id: Schema.Types.ObjectId,
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  site: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  id_manager: {
+    type: Schema.Types.ObjectId,
+    ref: "Employee",
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Service", ServiceSchema, "Service");
