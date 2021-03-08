a6f81ecd5888b8d6c686ca5fdb49bd08322fb82c
LHuartNisgroup <l.huart@nis-group.fr>
2021-03-08 23:19:31 +0100

    pb notifController résolu !!

--- src/controllers/NotificationController.js
+++ src/controllers/NotificationController.js
@@ -1,7 +1,6 @@
 const nodemailer = require("nodemailer");
 const HolidaySchema = require("../models/Holiday");
 const EmployeeSchema = require("../models/Employee");
-const Employee = require("../models/Employee");
 const { populate } = require("../models/Holiday");
 const date = new Date();
 
@@ -110,32 +109,6 @@ var NotificationController = {
     );
   },
 
-  async NewHolidayRequestToManager(HolidayId) {
-    Holiday = await HolidaySchema.findById(HolidayId).populate({
-      path: "id_requester_employee",
-      populate: {
-        path: "id_service",
-        populate: {
-          path: "id_manager",
-        },
-      },
-    });
-    let html = `<b>Une nouvelle demande de congé de ${
-      Holiday.id_requester_employee.lastName
-    } ${Holiday.id_requester_employee.firstName} du ${date.toLocaleDateString(
-      Holiday.starting_date
-    )} au ${date.toLocaleDateString(
-      Holiday.ending_date
-    )} est en attente de validation</b>`;
-    html += `<br>`;
-    html += `Demande de congé :`;
-    sendMail(
-      Holiday.id_requester_employee.id_service.id_manager.mail,
-      `Une nouvelle demande de congé de ${Holiday.id_requester_employee.lastName} ${Holiday.id_requester_employee.firstName} est en attente de validation`,
-      html
-    );
-  },
-
   async NewEmployeetoServiceToManager(EmployeeId) {
     Employee = await EmployeeSchema.findById(EmployeeId).populate({
       path: "id_service",
@@ -151,7 +124,7 @@ var NotificationController = {
     );
   },
 
-  async NewEmployeetoEmployee(EmployeeId, password) {
+  async NewEmployeeRegistedToEmployee(EmployeeId, password) {
     let Employee = await EmployeeSchema.findById(EmployeeId);
     let html = `Voici les principales informations concernant votre compte :</br>`;
 
@@ -179,78 +152,6 @@ var NotificationController = {
     );
   },
 
-  async HolidayRequestStatusUpdateToEmployee(HolidayId) {
-    Holiday = await HolidaySchema.findById(HolidayId).populate(
-      "id_requester_employee"
-    );
-
-    let html = `<b>Le status de votre demande de congé du ${date.toLocaleDateString(
-      Holiday.starting_date
-    )} au ${date.toLocaleDateString(Holiday.ending_date)} a changé de status: ${
-      Holiday.status
-    }</b>`;
-    html += `<br>`;
-    html += `Demande de congé :`;
-    sendMail(
-      Holiday.id_requester_employee.mail,
-      "Votre demande de congé a changé de status",
-      html
-    );
-  },
-
-  async HolidayRequestStatusUpdateToManager(HolidayId) {
-    Holiday = await HolidaySchema.findById(HolidayId).populate({
-      path: "id_requester_employee",
-      populate: {
-        path: "id_service",
-        populate: {
-          path: "id_manager",
-        },
-      },
-    });
-    let html = `<b>Le status de votre demande de congé de ${
-      Holiday.id_requester_employee.lastName
-    } ${Holiday.id_requester_employee.firstName} du ${date.toLocaleDateString(
-      Holiday.starting_date
-    )} au ${date.toLocaleDateString(Holiday.ending_date)} a changé de status: ${
-      Holiday.status
-    }</b>`;
-    html += `<br>`;
-    html += `Demande de congé :`;
-    sendMail(
-      Holiday.id_requester_employee.id_service.id_manager.mail,
-      `Le status de votre demande de congé de ${Holiday.id_requester_employee.lastName} ${Holiday.id_requester_employee.firstName} a changé`,
-      html
-    );
-  },
-
-  async NewHolidayRequestToRh(HolidayId) {
-    Holiday = await HolidaySchema.findById(HolidayId).populate(
-      "id_requester_employee"
-    );
-    EmployeeServiceRH = await EmployeeSchema.find({
-      id_service: process.env.ID_SERVICE_RH,
-    });
-
-    RHMail = await EmployeeServiceRH.map((RH) => {
-      return RH.mail;
-    });
-    let html = `<b>Une nouvelle demande de congé de ${
-      Holiday.id_requester_employee.lastName
-    } ${Holiday.id_requester_employee.firstName} du ${date.toLocaleDateString(
-      Holiday.starting_date
-    )} au ${date.toLocaleDateString(
-      Holiday.ending_date
-    )} est en attente de validation</b>`;
-    html += `<br>`;
-    html += `Demande de congé :`;
-    sendMail(
-      RHMail,
-      `Une nouvelle demande de congé de ${Holiday.id_requester_employee.lastName} ${Holiday.id_requester_employee.firstName} est en attente de validation`,
-      html
-    );
-  },
-
   async NewHolidayRequestToManager(HolidayId) {
     Holiday = await HolidaySchema.findById(HolidayId).populate({
       path: "id_requester_employee",
@@ -273,42 +174,6 @@ var NotificationController = {
     sendMail(
       Holiday.id_requester_employee.id_service.id_manager.mail,
       `Une nouvelle demande de congée de ${Holiday.id_requester_employee.lastName} ${Holiday.id_requester_employee.firstName} est en attente de validation`,
-      html
-    );
-  },
-
-  async NewEmployeetoServiceToManager(EmployeeId) {
-    Employee = await EmployeeSchema.findById(EmployeeId).populate({
-      path: "id_service",
-      populate: {
-        path: "id_manager",
-      },
-    });
-    let html = `<b>${Employee.lastName} ${Employee.firstName} vient de rejoindre votre service</b>`;
-    sendMail(
-      Employee.id_service.id_manager.mail,
-      `${Employee.lastName} ${Employee.firstName} vient de rejoindre votre service`,
-      html
-    );
-  },
-
-  async NewEmployeeRegistedToDirection(EmployeeId) {
-    Employee = await EmployeeSchema.findById(EmployeeId).populate({
-      path: "id_service",
-      populate: {
-        path: "id_manager",
-      },
-    });
-    EmployeeServiceDirection = await EmployeeSchema.find({
-      id_service: process.env.ID_SERVICE_DIRECTION,
-    });
-    DirectionMail = EmployeeServiceDirection.map((direction) => {
-      return direction.mail;
-    });
-    let html = `<b>Un nouvel employé vient d'etre créé, ${Employee.lastName} ${Employee.firstName}, il vient de rejoindre le service ${Employee.id_service.name} managé par ${Employee.id_service.id_manager.lastName} ${Employee.id_service.id_manager.firstName}</b>`;
-    sendMail(
-      DirectionMail,
-      `Un nouvel employé vient d'etre créé, ${Employee.lastName} ${Employee.firstName}`,
       html
     );
   },
